14d41b1d997bd144fd20a4daf021446f228f55d2
MohammedHittini <moh.hittini993@gmail.com>
2020-03-02 09:15:59 +0400

    Initial Commit

---
+++ SaifZoneNewApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaifZoneNewApplication
+//
+//  Created by macbook pro on 3/2/20.
+//  Copyright © 2020 Datacellme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
